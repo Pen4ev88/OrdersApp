62c7b09ae940d0ce45824cea69aae9ca13ab9b0e
Pen4ev88 <m.pen4ev88@gmail.com>
2021-05-20 00:09:38 +0300

    Final Commit

--- OrdersApp/OrdersApp/App.cs
+++ OrdersApp/OrdersApp/App.cs
@@ -18,22 +18,72 @@ namespace OrdersApp
 
         public async Task Run()
         {
-            var orders = await orderService.GetAllOrders();
+            List<OrdersBusinessLogic.Models.Order> orders = await orderService.GetAllOrders();
 
+            int startIndex = 0;
+            int endIndex = startIndex;
             while(true)
-            {
-                Console.WriteLine("Enter command N / P / A or Exit!");
+            { 
+                Console.WriteLine("Enter command N, P, A or Exit!");
                 string command = Console.ReadLine();
 
                 if(command == "Exit")
                 {
                     break;
                 }
-
-                else
+                
+                switch (command)
                 {
+                    case "A":
+                        {
+                            startIndex = orders.IndexOf(orders.First());
+                            endIndex = orders.IndexOf(orders.Last());
+                            PrintOrdersds(orders, startIndex, endIndex);
+                        } 
+                        break;
+                    case "N":
+                        {
+                            startIndex += 2;
+                            endIndex = startIndex +1;
 
+                            if(startIndex > (orders.IndexOf(orders.Last()) - 1) )
+                            {
+                                startIndex = orders.IndexOf(orders.First());
+                                endIndex = startIndex + 1;
+                            }
+                            PrintOrdersds(orders, startIndex, endIndex);
+                        }
+                        break;
+                    case "P":
+                        {
+                            endIndex = startIndex - 1;
+                            startIndex -= 2;                            
+
+                            if (startIndex < (orders.IndexOf(orders.First())))
+                            {
+                                startIndex = orders.IndexOf(orders.Last()) - 1;
+                                endIndex = startIndex + 1;
+                            }
+                            PrintOrdersds(orders, startIndex, endIndex);
+                        }
+                        break;
+
+                    default:
+                        break;
                 }
+            }
+        }
+
+        private void PrintOrdersds(List<OrdersBusinessLogic.Models.Order> orders, int starIndex, int endIndex)
+        {
+            for (int i = starIndex; i <= endIndex; i++)
+            {
+                Console.WriteLine(orders.ElementAt(i).Id);
+                Console.WriteLine(orders.ElementAt(i).Name);
+                Console.WriteLine(orders.ElementAt(i).Price);
+                Console.WriteLine(orders.ElementAt(i).Volume);
+                Console.WriteLine(orders.ElementAt(i).CreatedData);
+                Console.WriteLine();
             }
         }
     }
